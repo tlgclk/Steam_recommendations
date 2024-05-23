1b6419dcc52f7331db2f2f56f30135e8c588c0a1
tlgclk <tolga_cakal@hotmail.com>
2024-05-23 22:38:17 +0300

    sim_matrix.csv created

--- cos_sim_prep.py
+++ cos_sim_prep.py
@@ -5,21 +5,4 @@ from pandas import DataFrame
 
 df_user_ratings = pd.read_csv('user_ratings.csv')
 df_filter_user_ratings = df_user_ratings[['user_id','appid','Assuming_Ratings']]
-df_filter_user_ratings.to_csv('filter_user_ratings')
-
-df_filter_user_ratings_ = pd.read_csv('filter_user_ratings')
-df_filter_user_ratings_.head(5)
-df_filter_user_ratings_ = df_filter_user_ratings_.drop(['Unnamed: 0'], axis=1)
-
-a0 = pd.DataFrame()
-for i in range(8):
-    #a0 = user_rating_df.loc[user_rating_df['user_id']==0]
-    #a0 = a0.rename(columns={'Assuming_Ratings': 0})
-    #a0 = a0.drop(['user_id'], axis=1)
-#aa = a0.set_index('appid', inplace=True)
-    a1 = df_filter_user_ratings_.loc[df_filter_user_ratings_['user_id']==i]
-    a1 = a1.rename(columns={'Assuming_Ratings': 'ratings'})
-    #a1 = a1.drop(['user_id'], axis=1)
-#a1 = a1.set_index('appid', inplace=True)
-    
-    a0 = pd.concat([a0, a1], axis=0)
+df_filter_user_ratings.to_csv('cos_sim_prep.csv')
---
+++ sim_matrix.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial.distance import cosine
+from pandas import DataFrame
+
+df_filter_user_ratings_ = pd.read_csv('cos_sim_prep.csv')
+df_filter_user_ratings_ = df_filter_user_ratings_.drop(['Unnamed: 0'], axis=1)
+print(df_filter_user_ratings_.head(5))
+
+a0 = pd.DataFrame()
+for i in range(8):
+    a1 = df_filter_user_ratings_.loc[df_filter_user_ratings_['user_id']==i]
+    a1 = a1.rename(columns={'Assuming_Ratings': 'ratings'})
+    a0 = pd.concat([a0, a1], axis=0)
+
+df = a0.pivot(index='appid', columns='user_id', values='ratings')
+print(df.head(5))
+df.fillna(0.0, inplace = True)
+df.to_csv('df_fill_NA.csv')
+
+similarity_matrix = pd.DataFrame(index=range(8), columns=range(8))
+
+for i in range(8):
+    for j in range(8):
+        similarity_matrix.iloc[i, j] = 1 - cosine(df.iloc[:, i], df.iloc[:, j])
+        
+print(similarity_matrix)
+similarity_matrix.to_csv('sim_matrix.csv', index=True)
+        
